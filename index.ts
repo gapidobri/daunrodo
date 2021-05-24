7e7618f9f71e650d3b60ff7fb900c8b28a13cb14
Gašper Dobrovoljc <gasper@yourflare.io>
2021-05-24 11:05:20 +0200

    Fixed youtube-dl not working

--- index.ts
+++ index.ts
@@ -1,104 +1,116 @@
-import { Client, Message, MessageEmbed } from 'discord.js';
-import youtubedl, { Youtubedl } from 'youtube-dl';
-import progress, { ProgressStream } from 'progress-stream';
-import ffmpeg from 'fluent-ffmpeg';
-import path, { PlatformPath } from 'path';
-import fs, { WriteStream } from 'fs';
-import config from './config.json';
-import dotenv from 'dotenv';
-import { isForInStatement } from 'typescript';
-import { execFile } from 'child_process';
-import { setServers } from 'dns';
-dotenv.config();
+import { Client, Message, MessageEmbed } from 'discord.js'
+import youtubedl, { Youtubedl } from 'youtube-dl'
+import progress, { Progress } from 'progress-stream'
+import ffmpeg from 'fluent-ffmpeg'
+import path from 'path'
+import fs from 'fs'
+import config from './config.json'
+import dotenv from 'dotenv'
 
-const bot: Client = new Client();
+dotenv.config()
+
+try {
+  fs.mkdirSync('download')
+} catch (e) {}
+
+const bot: Client = new Client()
+let downloads: Array<String> = []
 
 bot.on('ready', () => {
-    console.log('Bot is online');
+  console.log('Bot is online')
 })
 
 bot.on('message', (msg) => {
-    const message: string = msg.content.trim();
-    const prefix: string = message.split('')[0];
-    const command = message.split(' ')[0].substring(1);
-    const args: string[] = message.split(' ');
-    args.shift();
-    if (prefix != config.prefix) return;
-    switch (command) {
-        case 'dl':
-            download(msg, args[0]);
-            break;
-    }
-});
+  const message: string = msg.content.trim()
+  const prefix: string = message.split('')[0]
+  const command = message.split(' ')[0].substring(1)
+  const args: string[] = message.split(' ')
+  args.shift()
+  if (prefix !== config.prefix) return
+  switch (command) {
+    case 'dl':
+      download(msg, args[0])
+      break
+  }
+})
 
 const download = async (message: Message, url: string) => {
-
-    const embed:MessageEmbed = new MessageEmbed()
+  downloads.push(url)
+  const embed: MessageEmbed = new MessageEmbed()
     .setTitle('Downloading')
     .setURL(url)
-    .setDescription('Preparing download');
-    const reply:Message = await message.channel.send(embed); 
+    .setDescription('Preparing download')
 
-    console.log(`Download requested by ${message.author.username}`);
+  const reply: Message = await message.channel.send(embed)
 
-    const video: Youtubedl = youtubedl(url, [], { cwd: __dirname });
+  console.log(`Download requested by ${message.author.username}`)
 
-    video.once('info', async (info) => {
-        const filenameLong:string = info._filename.replace('.mp4', '');
-        const filenameArr:string[] = filenameLong.split('-');
-        filenameArr.pop();
-        const filename:string = filenameArr.join('-');
+  const video: Youtubedl = youtubedl(url, [], { cwd: __dirname })
 
-        embed
-        .setTitle(`Downloading **${filename}**`)
-        .setURL(url)
-        .setDescription('Preparing download');
-        await reply.edit(embed);
+  video.once('info', async (info) => {
+    const filenameLong: string = info._filename.replace('.mp4', '')
+    const filenameArr: string[] = filenameLong.split('-')
+    filenameArr.pop()
+    const filename: string = filenameArr.join('-')
 
-        console.log(`Downloading ${filename}`);
+    embed
+      .setTitle(`Downloading **${filename}**`)
+      .setURL(url)
+      .setDescription('Preparing download')
+    await reply.edit(embed)
 
-        const str = progress({
-            length: info.size,
-            time: 500,
-        });
-    
-        str.on('progress', (prog) => {
-            const percentage:number = Math.round(prog.percentage)
-            const progressBar:string = makeProgressBar(percentage, 30);
-            embed.setDescription(`${progressBar} ${percentage} % / eta ${prog.eta} s`);
-            reply.edit(embed);
-            console.log(`Downloaded ${percentage} %`);
-        });
-        
-        const proc = ffmpeg({ source: video.pipe(str) });
+    console.log(`Downloading ${filename}`)
 
-        const filePath:string = path.join('download', filename + '.mp3');
-        proc.saveToFile(filePath);
+    const str = progress({
+      length: info.size,
+      time: 500,
+    })
 
-        video.on('close', async () => {
-            embed.setDescription('Download complete');
-            reply.edit(embed);
-            console.log('Download completed');
-            await message.channel.send({
-                files: [ filePath ],
-            });
-            fs.unlinkSync(filePath);
-        });
-    });
+    let prog: Progress
 
+    str.on('progress', async (p) => {
+      prog = p
+    })
+
+    const interval = setInterval(async () => {
+      if (!downloads.includes(url)) return
+      const percentage: number = Math.round(prog.percentage)
+      const progressBar: string = makeProgressBar(percentage, 30)
+      embed.setDescription(`${progressBar} ${percentage} % / eta ${prog.eta} s`)
+      console.log(`Downloaded ${percentage} %`)
+      await reply.edit(embed)
+
+      if (percentage === 100) {
+        downloads = downloads.filter((d) => d != url)
+        interval.unref()
+        embed.setDescription('Download complete')
+        console.log('Download completed')
+        await reply.edit(embed)
+        await message.channel.send({
+          files: [filePath],
+        })
+        fs.unlinkSync(filePath)
+      }
+    }, 2000)
+
+    const proc = ffmpeg({ source: video.pipe(str) })
+
+    const filePath: string = path.join('download', filename + '.mp3')
+    proc.saveToFile(filePath)
+
+    video.on('close', async () => {})
+  })
 }
 
-const makeProgressBar = (percent:number, sections:number): string => {
-    let progressBar:string = '';
-    const secPercent:number = 100 / sections;
-    const secAmount:number = Math.round(percent / secPercent);
-    for (let i:number = 0; i < sections; i++) {
-        if (i <= secAmount)
-            progressBar += '█';
-        else
-            progressBar += '▒';
-    }
-    return progressBar;
+const makeProgressBar = (percent: number, sections: number): string => {
+  let progressBar: string = ''
+  const secPercent: number = 100 / sections
+  const secAmount: number = Math.round(percent / secPercent)
+  for (let i: number = 0; i < sections; i++) {
+    if (i <= secAmount) progressBar += '█'
+    else progressBar += '▒'
+  }
+  return progressBar
 }
 
-bot.login(process.env.TOKEN);
+bot.login(process.env.TOKEN)
